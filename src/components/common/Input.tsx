2367449cdc0062692d259994f82230f4a5b7bb5f
Jordão Qualho <jordaoqualho@gmail.com>
2023-07-18 14:59:52 -0300

    fix: changing checkbox and input behaviour

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -3,18 +3,20 @@ import HttpsIcon from "@mui/icons-material/Https";
 import { Container } from "./InputStyle";
 
 type InputProps = {
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
   placeholder: string;
   autoFocus?: boolean;
   type?: string;
+  name: string;
 };
 
 export default function Input(props: InputProps) {
-  const { placeholder, type = "text", autoFocus = false } = props;
+  const { placeholder,name , type = "text", autoFocus = false, onChange } = props;
 
   return (
     <Container title="input">
       {type === "text" ? <AccountCircleIcon className="icon" /> : <HttpsIcon className="icon" />}
-      <input type={type} placeholder={placeholder} className="user" autoFocus={autoFocus} />
+      <input type={type} placeholder={placeholder} className="user" autoFocus={autoFocus} onChange={onChange} name={name} />
     </Container>
   );
 }
