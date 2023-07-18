e422db6f00ac949d26a2787e7e0df58c3a76d1a2
Jordão Qualho <jordaoqualho@gmail.com>
2023-07-18 10:42:06 -0300

    improve checkbox

--- src/components/common/Checkbox.tsx
+++ src/components/common/Checkbox.tsx
@@ -1,11 +1,26 @@
+import { useState } from "react";
 import { Container } from "./CheckboxStyle";
 
 export default function Checkbox() {
+  const [isChecked, setIsChecked] = useState<boolean>(true);
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      setIsChecked(!isChecked);
+    }
+  };
+
   return (
     <Container title="checkbox">
       <label className="control control--checkbox">
-        <input type="checkbox" className="checked" />
-        <div className="control__indicator"></div>
+        <input
+          type="checkbox"
+          className="checked"
+          checked={isChecked}
+          onClick={() => setIsChecked(!isChecked)}
+          onKeyDown={handleKeyDown}
+        />
+        <div className="control__indicator" />
       </label>
     </Container>
   );
