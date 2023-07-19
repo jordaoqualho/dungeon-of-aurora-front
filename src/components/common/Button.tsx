f1b86928079638a804d861f0d6d5135b19a691f5
Jordão Qualho <jordaoqualho@gmail.com>
2023-07-19 15:14:40 -0300

    feat(button.tsx): creating button component

---
+++ src/components/common/Button.tsx
@@ -0,0 +1,27 @@
+import { Container, Loader } from "./Button.styles";
+
+type IconType = {
+  src: string;
+  alt: string;
+};
+
+type ButtonProps = {
+  onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  type?: "button" | "submit" | "reset" | undefined;
+  icon?: IconType;
+  style?: React.CSSProperties;
+  loading?: boolean;
+  text: string;
+};
+
+export function Button(props: ButtonProps) {
+  const { onClick, type, style, text, loading, icon } = props;
+  return (
+    <Container>
+      <button onClick={onClick} type={type} style={style} className="flex_ccr">
+        {icon && <img src={icon.src} alt={icon.alt} />}
+        {loading ? <Loader /> : text ? text : "Salvar"}
+      </button>
+    </Container>
+  );
+}
