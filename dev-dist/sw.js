217455ab8ba48f7b73747fc3d4c8b018ea5d5b69
Jordão Qualho <jordaoqualho@gmail.com>
2023-07-21 17:04:41 -0300

    chore: improving the swc and pwa settings

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab7aa862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.omn9hmbiok8"
+    "revision": "0.54k8lijmkoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
