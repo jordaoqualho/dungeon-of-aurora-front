eac64ce0bfa8cd5d1275cc98424f1b30796811bd
Jordão Qualho <jordaoqualho@gmail.com>
2023-07-22 19:04:54 -0300

    feat: adding headers to useFetch hook

--- src/types/fetcher.ts
+++ src/types/fetcher.ts
@@ -1,5 +1,10 @@
 export type HttpMethod = "GET" | "POST" | "PUT" | "DELETE";
+export type HttpHeaders = {
+  Authorization?: string;
+  Accept?: string;
+};
 export type FetcherConfig = {
   method: HttpMethod;
   url: string;
+  headers?: HttpHeaders;
 };
