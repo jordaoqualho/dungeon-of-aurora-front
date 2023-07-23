8fe41c6983d122f7c8e044c82e020fd7189c74dc
Jordão Qualho <jordaoqualho@gmail.com>
2023-07-23 03:50:46 -0300

    feat(login-page): implementing google login authentication

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab7aa862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.54k8lijmkoo"
+    "revision": "0.r8omhavam8o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
