aad0188270601ae145a56fa23b3fb707b4654dd0
Jordão Qualho <jordaoqualho@gmail.com>
2023-07-24 15:37:20 -0300

    feat: creating signUp modal screen

---
+++ src/components/login/SignIn.tsx
@@ -0,0 +1,132 @@
+import { google_logo } from "@/assets";
+import { showToast } from "@/providers/toasterProvider";
+import { SignInData, SignInError } from "@/types";
+import { parseJwt } from "@/utils/jwtParser";
+import { useGoogleLogin, useGoogleOneTapLogin } from "@react-oauth/google";
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button } from "../common/Button";
+import Checkbox from "../common/Checkbox";
+import Input from "../common/Input";
+import { Divisor, Modal, Remember, google_btn, login_btn, signin_btn } from "./Sign.styles";
+
+export default function SignIn({ style, setSignShow }: any) {
+  const [signInData, setSignInData] = useState<SignInData>({ user: "", password: "" });
+  const [error, setError] = useState<SignInError>({ user: false, password: false });
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSignIn = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (loading) return showToast("Já esta carregando");
+    if (!validateFields()) return showToast("Preencha todos os campos!");
+    setLoading(true);
+    await validadeSignInInfo()
+      .then((res: any) => {
+        showToast(res.message, "success");
+        navigate("/home");
+      })
+      .catch((error) => showToast(error.message))
+      .finally(() => setLoading(false));
+  };
+
+  const validateFields = () => {
+    const { user, password } = signInData;
+
+    if (user === "" || password === "") {
+      setError((prevError) => ({ ...prevError, user: user === "", password: password === "" }));
+      return false;
+    }
+
+    return true;
+  };
+
+  const validadeSignInInfo = async () => {
+    const { user, password } = signInData;
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === "admin" && password === "admin") resolve({ success: true, message: "Você está logado!" });
+        reject({ success: false, message: "Usuário ou senha inválido" });
+      }, 1000);
+    });
+  };
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setSignInData({
+      ...signInData,
+      [name]: value,
+    });
+    setError((prevError) => ({ ...prevError, [name]: value === "" }));
+  };
+
+  const googleSignIn = useGoogleLogin({
+    onSuccess: (tokenResponse) => {
+      getGoogleProfile(tokenResponse.access_token);
+      showToast("Você está logado!", "success");
+      navigate("/home");
+    },
+    onError: () => {
+      showToast("Login com google falhou");
+    },
+  });
+
+  const getGoogleProfile = async (access_token: string) => {
+    console.log(access_token);
+
+    const url = `${access_token}`;
+    await axios
+      .get(url)
+      .then((res) => console.log(res.data))
+      .catch((error) => showToast(error));
+  };
+
+  useGoogleOneTapLogin({
+    onSuccess: (response) => {
+      console.log(parseJwt(response.credential));
+      showToast("Você está logado!", "success");
+      navigate("/home");
+    },
+    onError: () => {
+      showToast("Login com google falhou");
+    },
+  });
+
+  return (
+    <Modal title="modal" onSubmit={handleSignIn} style={...style}>
+      <div className="title">
+        <h1>Eae blz? 👋</h1>
+        <p>Bota suas informações de login aqui embaixo pra entrar.</p>
+      </div>
+      <Input placeholder="Seu usuário" onChange={handleChange} name="user" autoFocus inputError={error.user} />
+      <Input
+        type="password"
+        placeholder="Sua senha"
+        onChange={handleChange}
+        name="password"
+        inputError={error.password}
+      />
+      <Remember title="remember" className="flex_sbr">
+        <div className="check-box  flex_ssr">
+          <Checkbox />
+          <label htmlFor="">Continuar conectado</label>
+        </div>
+        <a href="">Esqueceu a senha?</a>
+      </Remember>
+      <Button style={login_btn} type="submit" text="Entrar" loading={loading} />
+      <Button style={signin_btn} text="Criar uma conta" onClick={() => setSignShow("SignUp")} />
+      <Divisor title="divisor" className="flex_ccr">
+        <div className="line" />
+        <span>ou</span>
+        <div className="line" />
+      </Divisor>
+      <Button
+        icon={{ src: google_logo, alt: "google_logo" }}
+        onClick={() => googleSignIn()}
+        text="Entrar com Google"
+        style={google_btn}
+      />
+    </Modal>
+  );
+}
--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,117 +1,12 @@
-import { google_logo } from "@/assets";
 import castle from "@/assets/images/castle.png";
-import { Button } from "@/components/common/Button";
-import Checkbox from "@/components/common/Checkbox";
-import Input from "@/components/common/Input";
 import TiltBox from "@/components/common/TiltBox";
-import {
-  Container,
-  Divisor,
-  Form,
-  Modal,
-  Remember,
-  google_btn,
-  login_btn,
-  signin_btn,
-} from "@/pages/login/Login.styles";
-import { showToast } from "@/providers/toasterProvider";
-import { parseJwt } from "@/utils/jwtParser";
-import { useGoogleLogin, useGoogleOneTapLogin } from "@react-oauth/google";
-import axios from "axios";
+import SignIn from "@/components/login/SignIn";
+import SignUp from "@/components/login/SignUp";
+import { Container } from "@/pages/login/Login.styles";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-type LoginData = {
-  user: string;
-  password: string;
-};
-
-type LoginError = {
-  user: boolean;
-  password: boolean;
-};
 
 export default function Login() {
-  const [loginData, setLoginData] = useState<LoginData>({ user: "", password: "" });
-  const [error, setError] = useState<LoginError>({ user: false, password: false });
-  const [loading, setLoading] = useState(false);
-  const navigate = useNavigate();
-
-  const handleLogin = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (loading) return showToast("Já esta carregando");
-    if (!validateFields()) return showToast("Preencha todos os campos!");
-    setLoading(true);
-    await validadeLogin()
-      .then((res: any) => {
-        showToast(res.message, "success");
-        navigate("/home");
-      })
-      .catch((error) => showToast(error.message))
-      .finally(() => setLoading(false));
-  };
-
-  const validateFields = () => {
-    const { user, password } = loginData;
-
-    if (user === "" || password === "") {
-      setError((prevError) => ({ ...prevError, user: user === "", password: password === "" }));
-      return false;
-    }
-
-    return true;
-  };
-
-  const validadeLogin = async () => {
-    const { user, password } = loginData;
-    return new Promise((resolve, reject) => {
-      setTimeout(() => {
-        if (user === "admin" && password === "admin") resolve({ success: true, message: "Você está logado!" });
-        reject({ success: false, message: "Usuário ou senha inválido" });
-      }, 1000);
-    });
-  };
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    setLoginData({
-      ...loginData,
-      [name]: value,
-    });
-    setError((prevError) => ({ ...prevError, [name]: value === "" }));
-  };
-
-  const loginWithGoogle = useGoogleLogin({
-    onSuccess: (tokenResponse) => {
-      getGoogleProfile(tokenResponse.access_token);
-      showToast("Você está logado!", "success");
-      navigate("/home");
-    },
-    onError: () => {
-      showToast("Login com google falhou");
-    },
-  });
-
-  const getGoogleProfile = async (access_token: string) => {
-    console.log(access_token);
-
-    const url = `${access_token}`;
-    await axios
-      .get(url)
-      .then((res) => console.log(res.data))
-      .catch((error) => showToast(error));
-  };
-
-  useGoogleOneTapLogin({
-    onSuccess: (response) => {
-      console.log(parseJwt(response.credential));
-      showToast("Você está logado!", "success");
-      navigate("/home");
-    },
-    onError: () => {
-      showToast("Login com google falhou");
-    },
-  });
+  const [signShow, setSignShow] = useState("signIn");
 
   // const logOut = () => {
   //   googleLogout();
@@ -121,49 +16,16 @@ export default function Login() {
   return (
     <Container title="container">
       <div className="w100 hfs flex_ccc">
-        <Form title="form" className="flex_ssr" onSubmit={handleLogin}>
-          <Modal title="modal">
-            <div className="title">
-              <h1>Eae blz? 👋</h1>
-              <p>Bota suas informações de login aqui embaixo pra entrar.</p>
-            </div>
-            <Input placeholder="Seu usuário" onChange={handleChange} name="user" autoFocus inputError={error.user} />
-            <Input
-              type="password"
-              placeholder="Sua senha"
-              onChange={handleChange}
-              name="password"
-              inputError={error.password}
-            />
-            <Remember title="remember" className="flex_sbr">
-              <div className="check-box  flex_ssr">
-                <Checkbox />
-                <label htmlFor="">Continuar conectado</label>
-              </div>
-              <a href="">Esqueceu a senha?</a>
-            </Remember>
-            <Button style={login_btn} type="submit" text="Entrar" loading={loading} />
-            <Button
-              style={signin_btn}
-              text="Criar uma conta"
-              onClick={() => showToast("Cadastro indisponível", "warning")}
-            />
-            <Divisor title="divisor" className="flex_ccr">
-              <div className="line" />
-              <span>ou</span>
-              <div className="line" />
-            </Divisor>
-            <Button
-              icon={{ src: google_logo, alt: "google_logo" }}
-              onClick={() => loginWithGoogle()}
-              text="Entrar com Google"
-              style={google_btn}
-            />
-          </Modal>
+        <div className="flex_ssr">
+          <SignIn
+            setSignShow={(value: string) => setSignShow(value)}
+            style={{ display: signShow === "signIn" ? "block" : "none" }}
+          />
+          <SignUp style={{ display: signShow === "SignUp" ? "block" : "none" }} />
           <TiltBox>
             <img className="castle_img" src={castle} alt="castle" />
           </TiltBox>
-        </Form>
+        </div>
       </div>
     </Container>
   );
