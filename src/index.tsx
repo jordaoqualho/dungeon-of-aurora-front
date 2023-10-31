44e94a1d4ad5c3fe2213015a846908d0d89eb3f7
Jordão Qualho <jordaoqualho@gmail.com>
2023-10-31 10:01:11 -0300

    fix: 🐛 app style upper case problem

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-import { AppContainer } from "@/styles/App.styles";
+import { AppContainer } from "@/styles/app.styles";
 import darkTheme from "@/styles/darkTheme";
 import GlobalStyle from "@/styles/global.styles";
 
