fdd9780027de7fea1102d9e74dfb1a975ce34a51
Jordão Qualho <jordaoqualho@gmail.com>
2023-11-01 23:10:24 -0300

    feat: 🎸 finishing functional login and register

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab7aa862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q5t4l3t1nm8"
+    "revision": "0.tu439uos12"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
