48e64b55811d8f9813e1c4185ff5e9e5b62ad2ad
Jordão Qualho <jordaoqualho@gmail.com>
2023-11-05 04:57:33 -0300

    feat: 🎸 improve services response data

--- src/connection/userService.ts
+++ src/connection/userService.ts
@@ -1,14 +1,20 @@
 import { backend } from '@/config';
 import { ServiceInterface } from '@/interfaces';
-import { User } from '@/types';
+import { ResponseAxios, User } from '@/types';
 
 class UserService implements ServiceInterface {
-  async post(data: User): Promise<void> {
-      return await backend.post(`/user`, data);
+  async post(data: User): Promise<User> {
+    const response =  await backend.post<ResponseAxios<User>>(`/user`, data);
+    return response.data.data;
   }
 
   async get(): Promise<void> {
       return await backend.get(`/user`);
+  }
+
+  async getByEmail(email: string): Promise<User> {
+      const response = await backend.get<ResponseAxios<User>>(`/user/email/${email}`);
+      return response.data.data;
   }
 }
 
