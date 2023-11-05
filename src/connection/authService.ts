ecce4711b03419bf0b7c88d9d0bf022f33abcb9d
Jordão Qualho <jordaoqualho@gmail.com>
2023-11-05 04:57:33 -0300

    feat: 🎸 improve services response data

--- src/connection/authService.ts
+++ src/connection/authService.ts
@@ -1,14 +1,12 @@
-import { backend } from '@/config';
-import { AuthInterface } from '@/interfaces';
-import { LoginData } from '@/types';
+import { backend } from "@/config";
+import { AuthInterface } from "@/interfaces";
+import { LoginData, LoginResponse, ResponseAxios } from "@/types";
 
 class AuthService implements AuthInterface {
-
-  async login({password, email}: LoginData): Promise<void> {
-    return await backend.post(`/auth/login`, 
-    {password, email});
+  async login({ password, email }: LoginData): Promise<LoginResponse> {
+    const response = await backend.post<ResponseAxios<LoginResponse>>(`/auth/login`, { password, email });
+    return response.data.data;
   }
-
 }
 
 export const authService: AuthInterface = new AuthService();
