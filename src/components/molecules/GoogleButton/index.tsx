1f2816094a82f82b70defbef989b22c745e80e4f
Jordão Qualho <jordaoqualho@gmail.com>
2023-11-05 05:08:17 -0300

    feat: 🎸 finish google login functionality

--- src/components/molecules/GoogleButton/index.tsx
+++ src/components/molecules/GoogleButton/index.tsx
@@ -1,46 +1,78 @@
 import { google_logo } from "@/assets";
 import { Button } from "@/components";
+import { userService } from "@/connection";
+import { useLocalStorage } from "@/hooks/useLocalStore";
 import { showToast } from "@/providers";
-import { ApiResponse } from "@/types";
-import { parseJwt } from "@/utils/jwtParser";
-import { TokenResponse, useGoogleLogin, useGoogleOneTapLogin } from "@react-oauth/google";
+import { ApiResponse, GoogleResponse, User } from "@/types";
+import { TokenResponse, useGoogleLogin } from "@react-oauth/google";
 import axios from "axios";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 export function GoogleButton() {
   const navigate = useNavigate();
-
-  const successLogin = () => {
-    showToast("Você está logado!", "success");
-    navigate("/home");
-  }
+  const [user, setUser] = useLocalStorage<User>("user");
 
   const failedLogin = () => {
     showToast("Login com google falhou");
-  }
+  };
 
   const googleButtonLogin = useGoogleLogin({
     onSuccess: (tokenResponse: TokenResponse) => {
       getGoogleProfile(tokenResponse.access_token);
-      successLogin();
     },
     onError: failedLogin,
   });
+
+  const successLogin = () => {
+    showToast(`Você está logado ${user?.name}`, "success");
+    navigate("/home");
+  };
 
   const getGoogleProfile = (access_token: string) => {
     axios
       .get(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${access_token}`)
-      .then((res) => console.log(res.data))
+      .then(async (response: GoogleResponse) => await handleUserLogin(response.data.email, response))
       .catch((error: ApiResponse) => showToast(error.message));
   };
 
-  useGoogleOneTapLogin({
-    onSuccess: (response) => {
-      console.log(parseJwt(response.credential));
-      successLogin();
-    },
-    onError: failedLogin,
-  });
+  const handleUserLogin = async (email: string, response: GoogleResponse) => {
+    const foundUser = await userService.getByEmail(email);
+    if (foundUser) {
+      setUser(foundUser);
+    } else {
+      await createUser(response);
+    }
+  };
+
+  const createUser = async (userData: GoogleResponse) => {
+    const { email, name, picture } = userData.data;
+    const formatedUser: User = {
+      email,
+      name,
+      avatarUrl: picture,
+      password: email,
+    };
+
+    await userService
+      .post(formatedUser)
+      .then((response) => setUser(response))
+      .catch((error) => console.log(error));
+  };
+
+  // useGoogleOneTapLogin({
+  //   onSuccess: (response) => {
+  //     const credentials = parseJwt(response.credential) as GoogleCredentials;
+  //     console.log(credentials);
+  //     // successLogin();
+  //   },
+  //   onError: failedLogin,
+  // });
+
+  useEffect(() => {
+    if (!user) return;
+    successLogin();
+  }, [user]);
 
   return (
     <Button
