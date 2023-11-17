2538b18349a037003ab7c0a636a8060efe19a116
Jordão Qualho <jordaoqualho@gmail.com>
2023-11-17 00:06:10 -0300

    feat: 🎸 add control button modal

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ab7aa862'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ogpbbf1e5mo"
+    "revision": "0.18ik5dsth4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
