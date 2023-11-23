5418e472aef30d467acdcb1e9aea1647e838b982
Jordão Qualho <jordaoqualho@gmail.com>
2023-11-23 11:15:30 -0300

    feat: 🎸 creating input for spell modal

--- src/components/molecules/SpellModal/styles.ts
+++ src/components/molecules/SpellModal/styles.ts
@@ -4,24 +4,10 @@ export const Container = styled.div`
   flex-wrap: wrap;
   gap: 16px;
 
-  .buttons {
-    gap: 16px;
-
-    button {
-      border-radius: 10px;
-      color: var(--white);
-      padding: 8px 24px;
-      min-width: 100px;
-      width: 100%;
-    }
-
-    .save {
-      background-color: var(--primary);
-    }
-
-    .cancel {
-      background-color: var(--border);
-    }
+  .search_input {
+    background-color: var(--background);
+    padding: 8px 16px;
+    border-radius: 10px;
   }
 `;
 
