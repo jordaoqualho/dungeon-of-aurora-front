39021fedd4df4acc20e3783aad12b6b6c65052fd
Jordão Qualho <jordaoqualho@gmail.com>
2023-11-23 11:15:30 -0300

    feat: 🎸 creating input for spell modal

--- src/components/molecules/SpellModal/index.tsx
+++ src/components/molecules/SpellModal/index.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Modal } from "@/components";
+import { useState } from "react";
 import { Container, modalStyles } from "./styles";
 
 type SpellModalProps = {
@@ -7,12 +8,24 @@ type SpellModalProps = {
 };
 
 export const SpellModal = (props: SpellModalProps) => {
+  const [search, setSearch] = useState("");
   const { isOpen } = props;
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
 
   return (
     <Modal isOpen={isOpen} customStyle={modalStyles}>
       <Container className="flex_ccc">
         <h4>Truques e magias</h4>
+        <input
+          type="text"
+          className="search_input"
+          placeholder="Digite o nome da magia ou truque"
+          value={search}
+          onChange={handleInputChange}
+        />
       </Container>
     </Modal>
   );
