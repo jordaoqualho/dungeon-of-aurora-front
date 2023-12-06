c952da9209f49d6cd76c345f8ae748ced250b022
Jordão Qualho <jordaoqualho@gmail.com>
2023-12-06 15:30:41 -0300

    feat: 🎸 add d20 icons
    ✅ Closes: DA-22

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,4 +1,6 @@
 import d20 from "./d20.svg";
+import d20_white from "./d20_white.svg";
 import google_logo from "./google_logo.svg";
 
-export { d20, google_logo };
+export { d20, d20_white, google_logo };
+
