62f913a7faff82179b03bf68214cce2b69a2d8d3
Jordão Qualho <jordaoqualho@gmail.com>
2023-12-07 23:05:15 -0300

    feat: 🎸 adding hexagon images
    ✅ Closes: DA-22

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,6 +1,8 @@
 import d20 from "./d20.svg";
 import d20_white from "./d20_white.svg";
 import google_logo from "./google_logo.svg";
+import hexagon_border from "./hexagon_border.svg";
+import hexagon_primary from "./hexagon_primary.svg";
 
-export { d20, d20_white, google_logo };
+export { d20, d20_white, google_logo, hexagon_border, hexagon_primary };
 
