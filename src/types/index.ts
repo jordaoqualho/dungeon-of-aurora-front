8219a7328f245c146433ebd998bcbe8ceec14475
Jordão Qualho <jordaoqualho@gmail.com>
2024-01-23 00:04:41 -0300

    feat: 🎸 creating equipment types and add to character

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,9 @@
-export * from "./character"
-export * from "./components"
-export * from './database'
-export * from './enviroment'
-export * from "./fetcher"
-export * from "./login"
-export * from "./themes"
+export * from "./character";
+export * from "./components";
+export * from "./database";
+export * from "./enviroment";
+export * from "./equipment";
+export * from "./fetcher";
+export * from "./login";
 export * from "./spell";
-
+export * from "./themes";
