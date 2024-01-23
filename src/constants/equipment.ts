b28a50b47be6870499ceebfb9b4f11aac5bd8d6e
Jordão Qualho <jordaoqualho@gmail.com>
2024-01-23 16:31:12 -0300

    feat: 🎸 adding constant icons for equipment

--- src/constants/equipment.ts
+++ src/constants/equipment.ts
@@ -1,3 +1,13 @@
+import {
+  adventure_icon,
+  armor_icon,
+  mount_icon,
+  potion_icon,
+  ranged_weapon_icon,
+  tool_icon,
+  valuable_icon,
+  weapon_icon,
+} from "@/assets";
 import { DamageTypes, Equipment, Pieces } from "@/types";
 
 export const initialEquipment: Equipment = {
@@ -24,9 +34,25 @@ export const initialEquipment: Equipment = {
   range: { normal: 2 },
   throwRange: { normal: 10, long: 20 },
   speed: { unit: "", quantity: 0 },
-  damage: { type: DamageTypes.CONTUNDENTE, dice: { quantity: 0, dice: "" } },
+  damage: { type: DamageTypes.CONTUNDENTE, dice: { quantity: 0, type: "" } },
   twoHandedDamage: {
     type: DamageTypes.CONTUNDENTE,
-    dice: { quantity: 0, dice: "" },
+    dice: { quantity: 0, type: "" },
   },
+};
+
+export type EquipmentIconMap = {
+  [key: string]: string;
+};
+
+export const equipmentIcons: EquipmentIconMap = {
+  Poção: potion_icon,
+  ArmaADistancia: ranged_weapon_icon,
+  Ferramentas: tool_icon,
+  Tesouros: valuable_icon,
+  Armas: weapon_icon,
+  Armaduras: armor_icon,
+  Aventura: adventure_icon,
+  Montaria: mount_icon,
+  default: potion_icon,
 };
