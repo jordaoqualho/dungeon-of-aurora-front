63fc27f41aa063426b1a346618cc589c16799a9f
Jordão Qualho <jordaoqualho@gmail.com>
2024-01-23 16:30:43 -0300

    style: 💄 adding equipment category icons

--- src/assets/svgs/index.ts
+++ src/assets/svgs/index.ts
@@ -1,4 +1,6 @@
 import abjuration_icon from "./abjuration_icon.svg";
+import adventure_icon from "./adventure_icon.svg";
+import armor_icon from "./armor_icon.svg";
 import conjuration_icon from "./conjuration_icon.svg";
 import d20 from "./d20.svg";
 import d20_white from "./d20_white.svg";
@@ -9,11 +11,19 @@ import google_logo from "./google_logo.svg";
 import hexagon_border from "./hexagon_border.svg";
 import hexagon_primary from "./hexagon_primary.svg";
 import ilusion_icon from "./ilusion_icon.svg";
+import mount_icon from "./mount_icon.svg";
 import necromancy_icon from "./necromancy_icon.svg";
+import potion_icon from "./potion_icon.svg";
+import ranged_weapon_icon from "./ranged_weapon_icon.svg";
+import tool_icon from "./tool_icon.svg";
 import transmutation_icon from "./transmutation_icon.svg";
+import valuable_icon from "./valuable_icon.svg";
+import weapon_icon from "./weapon_icon.svg";
 
 export {
   abjuration_icon,
+  adventure_icon,
+  armor_icon,
   conjuration_icon,
   d20,
   d20_white,
@@ -24,6 +34,13 @@ export {
   hexagon_border,
   hexagon_primary,
   ilusion_icon,
+  mount_icon,
   necromancy_icon,
+  potion_icon,
+  ranged_weapon_icon,
+  tool_icon,
   transmutation_icon,
+  valuable_icon,
+  weapon_icon,
 };
+
