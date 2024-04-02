7d8b025a75cbe054bced65639a3925b439ad5a1f
Jordão Qualho <jordaoqualho@gmail.com>
2024-04-01 23:19:43 -0300

    feat: 🎸 improving equipment description modal

--- src/components/molecules/EquipmentDescritionModal/index.tsx
+++ src/components/molecules/EquipmentDescritionModal/index.tsx
@@ -30,10 +30,10 @@ export const EquipmentDescritionModal = (
           return partIndex % 2 === 0 ? (
             <React.Fragment key={partIndex}>{part}</React.Fragment>
           ) : (
-            <>
+            <React.Fragment key={partIndex}>
               <br />
-              <strong key={partIndex}>{part}</strong>
-            </>
+              <strong>{part}</strong>
+            </React.Fragment>
           );
         });
 
@@ -51,12 +51,89 @@ export const EquipmentDescritionModal = (
     return <span>{sections}</span>;
   };
 
+  const renderCategory = () => {
+    const { category, armorCategory, weaponCategory } = equipment;
+
+    let categoryString = category;
+    if (armorCategory) {
+      categoryString += ` ${armorCategory}`;
+    } else if (weaponCategory) {
+      categoryString += ` ${weaponCategory}`;
+    }
+
+    return <span>{categoryString}</span>;
+  };
+
+  const renderValues = () => {
+    const { damage, twoHandedDamage, armorClass } = equipment;
+
+    if (damage) {
+      const { quantity, type } = damage.dice;
+      const damageString = twoHandedDamage
+        ? `${quantity}${type}/${twoHandedDamage.dice.quantity}${twoHandedDamage.dice.type}`
+        : `${quantity}${type}`;
+      return (
+        <h5 className="text">
+          Dano:{" "}
+          <span>
+            {damageString} ({damage.type})
+          </span>
+        </h5>
+      );
+    }
+    if (armorClass) {
+      const { base, dex_bonus, max_bonus } = armorClass;
+      const dexBonusString = dex_bonus
+        ? `+ Mod. Dextreza ${max_bonus ? `(Máx. +${max_bonus})` : ""}`
+        : "";
+      return (
+        <h5 className="text">
+          CA:{" "}
+          <span>
+            {base} {dexBonusString}
+          </span>
+        </h5>
+      );
+    }
+    return null;
+  };
+
+  const renderProperties = () => {
+    const { properties } = equipment;
+
+    if (properties) {
+      const propertiesString = properties.join(", ");
+
+      return (
+        <h5 className="text">
+          Propriedades: <span>{propertiesString}</span>
+        </h5>
+      );
+    }
+  };
+
+  const renderRange = () => {
+    const { range } = equipment;
+
+    if (range) {
+      return (
+        <h5 className="text">
+          Alcance:{" "}
+          <span>
+            {range.normal}
+            {range?.long ? `/${range.long}` : ""} Quad.
+          </span>
+        </h5>
+      );
+    }
+  };
+
   return (
     <Modal isOpen={isOpen} customStyle={modalStyles}>
       <Container className="flex_ccs">
         <div className="title">
           <h4>{equipment.name}</h4>
-          <span>{equipment.category}</span>
+          <span>Inglês: {equipment.originalName}</span>
         </div>
 
         <button className="close_btn flex_ccc" onClick={() => onClose()}>
@@ -69,14 +146,39 @@ export const EquipmentDescritionModal = (
             {equipment.cost.quantity} {equipment.cost.unit}
           </span>
         </h5>
+
         <h5 className="text">
           Peso: <span>{equipment.weight} Kg</span>
         </h5>
-        {equipment?.description && equipment.description.length > 0 && (
-          <h5 className="text description">
-            Descrição: {renderEquipmentDescription(equipment.description)}
-          </h5>
+
+        {renderValues()}
+
+        <h5 className="text">Categoria: {renderCategory()}</h5>
+
+        {equipment?.stealthDisadvantage && (
+          <h5 className="text">Desvantagem em Furtividade</h5>
         )}
+
+        {renderProperties()}
+
+        {renderRange()}
+        <h5 className="text">
+          Descrição <br />{" "}
+        </h5>
+        <div className="description">
+          {equipment?.description && equipment.description.length > 0 && (
+            <h5 className="text">
+              {renderEquipmentDescription(equipment.description)}
+            </h5>
+          )}
+          <br />
+          {equipment?.special && equipment.special.length > 0 && (
+            <h5 className="text">
+              Características Especiais
+              {renderEquipmentDescription(equipment.special)}
+            </h5>
+          )}
+        </div>
       </Container>
     </Modal>
   );
