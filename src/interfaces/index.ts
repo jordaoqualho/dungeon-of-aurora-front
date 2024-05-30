8d9576fa61e57d18112de4854fdfd38d500b23fb
Jordão Qualho <jordaoqualho@gmail.com>
2024-05-30 18:09:46 -0300

    feat: 🎸 creating features connection and interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,6 @@
 export * from "./anotation";
 export * from "./auth";
 export * from "./character";
+export * from "./feature";
 export * from "./user";
+
---
+++ src/types/feature.ts
@@ -0,0 +1,9 @@
+export type FeatureType = {
+  _id?: string;
+  name: string;
+  originalName: string;
+  class: string;
+  subClass?: string;
+  description: string[];
+  level: number;
+};
